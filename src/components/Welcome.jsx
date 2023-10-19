3c382ace6f148de055de43aaddc59791c82f34a9
Dafne Aquino <daquino@sparxworks.com>
2023-10-19 15:04:54 -0500

    update kitchen component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 import logo from "../images/logo.png";
-import { useHistory } from 'react-router';
-import './Welcome.css'
+import { useHistory } from "react-router";
+import "./Welcome.css";
 
 export function Welcome() {
-    const history=useHistory();
+  const history = useHistory();
   return (
     <div>
-        <section className='welcomeSection welcome'>
-            <div> Welcome to Burger Place! Click to begin</div>
-            <br/>
-            <img onClick={()=>{history.push('/users')}}src={logo} className='logoImage' alt='burgerLogo' style={{width:'auto',height: '150px', cursor:'pointer'}}/>
-        </section>
+      <section className="welcomeSection welcome">
+        <div> Welcome to Burger Place! Click to begin</div>
+        <br />
+        <img
+          onClick={() => {
+            history.push("/users");
+          }}
+          src={logo}
+          className="logoImage"
+          alt="burgerLogo"
+          style={{ width: "auto", height: "150px", cursor: "pointer" }}
+        />
+      </section>
     </div>
-  )
+  );
 }
